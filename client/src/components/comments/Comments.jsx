35cb02516f229d630f17e97dc943ef6b2ea079ab
tamar <asashvili@gmail.com>
2019-07-10 16:36:05 +0200

    users comments

--- client/src/components/comments/Comments.jsx
+++ client/src/components/comments/Comments.jsx
@@ -1,35 +1,33 @@
 import React, { Component } from "react";
-const comments = require("../../dummy-data/comments.json");
 
 class Comments extends Component {
   constructor() {
     super();
     this.state = {
-      comments: ""
+      comments: [{}]
     };
   }
   async componentDidMount() {
-    const comment = await fetch("http://localhost:3000/users/1").then(function(
-      response
-    ) {
-      console.log(response);
-      return response.json();
-    });
+    const comment = await fetch("http://localhost:3000/comment/1").then(
+      function(response) {
+        console.log(response);
+        return response.json();
+      }
+    );
     this.setState({ comments: comment });
   }
 
   render() {
-    return comments.map(x => (
+    return this.state.comments.map(x => (
       <div className="col-md-6">
         <div className="card mb-4 shadow-sm">
           <div className="card-body">
             <p className="card-text">
-              <h1>{this.state.comments}</h1>
               <strong>
                 <i className="fa fa-car" /> Comments:
               </strong>
-              <h3>hello: {this.state.comments}</h3>
-              <p> {x.body} </p>
+              {/* <h3>hello: {this.state.comments}</h3> */}
+              <p> {x.message} </p>
             </p>
           </div>
         </div>
