df1c690e933269d031bc794924605b5c3bb206d2
tamar <asashvili@gmail.com>
2019-07-10 11:34:21 +0200

    image

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark mb-5 navbar-fixed-top">
+      <span className="navbar mb-0 h2 mx-auto"> Open Recognition </span>
+      <span className="navbar mb-0 h2 mx-auto">
+        <button type="button" className="btn btn-primary">
+          Trust
+        </button>
+      </span>
+      <span className="navbar mb-0 h2 mx-auto">
+        <button type="button" className="btn btn-primary">
+          Trust
+        </button>
+      </span>
+      <div classclass="container" />
+    </nav>
+  );
+};
+export default Header;
