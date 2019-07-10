ba482fd5d900f0222524d3587b2e3faf4e90c55b
steevjunior <bitomagira@gmail.com>
2019-07-10 16:55:11 +0200

    first commit

--- demo-api-server/users/controller.js
+++ demo-api-server/users/controller.js
@@ -3,8 +3,6 @@ const usertable = require("./model");
 usertable.createTable();
 const controller = {};
 
-
-
 controller.insert = (req, res) => {
   const data = req.body;
   usertable.insert(data);
@@ -18,6 +16,20 @@ controller.getUser = (req, res) => {
     const data = user.rows;
     console.log(data);
     res.send(data);
+  });
+};
+
+controller.check = (req, res) => {
+  const login = req.params.login;
+  const password = req.params.password;
+  const resultat = usertable.check(login, password);
+  resultat.then(x => {
+    console.log(x);
+    if (x.rows.length === 0) {
+      res.status(403).send("error server");
+    } else {
+      res.send(x.rows);
+    }
   });
 };
 
