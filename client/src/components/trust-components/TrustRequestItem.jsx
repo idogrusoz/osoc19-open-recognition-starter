dede50f7c02956d35eab33d451fc547ecbff4a8a
ibrahim doğrusöz <ibrahimdogrusoz@ibrahims-MacBook-Pro.local>
2019-07-22 11:07:32 +0200

    trust issues

--- client/src/components/trust-components/TrustRequestItem.jsx
+++ client/src/components/trust-components/TrustRequestItem.jsx
@@ -1,45 +1,63 @@
 import React from "react";
-import { Card, Button } from 'react-bootstrap'
-import { Col, Row } from 'react-bootstrap'
-
+import { Card, Button } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 function TrustRequestItem(props) {
-    
-    const handleAccept = () => {
-        const dateApproving = new Date()
-        const id = localStorage.getItem('id')
-        const data = {
-            userrequesting: `${props.item.userrequesting}`,
-            userrecieving: id,
-            dateapproving: dateApproving
-        }
-        console.log(data)
-        fetch(`http://localhost:3000/trust/approve/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify(data),
-            headers: { "Content-Type": "application/json" }
-        })
-        .then(res => console.log("Trust request is approved", res))
-        .catch(err => console.log("Error:", err))
-    }
+  const handleAccept = () => {
+    const dateApproving = new Date();
+    const id = localStorage.getItem("id");
+    const data = {
+      userrequesting: `${props.item.id}`,
+      userrecieving: parseInt(id),
+      dateapproving: dateApproving
+    };
+    console.log(data);
+    fetch(`http://localhost:3000/trust/approve/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(res => console.log("Trust request is approved", res))
+    .catch(err => console.log("Error:", err))
+  };
 
+  const handleReject = async () => {
+    const id = localStorage.getItem("id");
+    const data = {
+      userrequesting: `${props.item.id}`,
+      userrecieving: parseInt(id)
+    };
+    await fetch(`http://localhost:3000/trust/reject/${id}`, {
+      method: "DELETE",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    })
+    .then(res => {console.log("Trust request is rejected", res) })
+    .catch(err => console.log("Error:", err));
+  };
 
-      return (
-          <Card>
-        <Card.Body>{console.log(props.item)}
-          <Card.Text>{props.item.first_name} {props.item.last_name} trusts you</Card.Text>
-         <Row>
-            <Col>
-          <Button variant="primary" size="sm" onClick={handleAccept}>Accept</Button>
+  return (
+    <Card>
+      <Card.Body>
+        {console.log(props.item)}
+        <Card.Text>
+          {props.item.first_name} {props.item.last_name} trusts you
+        </Card.Text>
+        <Row>
+          <Col>
+            <Button variant="primary" size="sm" onClick={() => {handleAccept(); props.handleUpdate(props.item.id)}}>
+              Accept
+            </Button>
           </Col>
           <Col>
-          <Button variant="primary"size="sm">Reject</Button>
+            <Button variant="primary" size="sm" onClick={() => {handleReject(); props.handleUpdate(props.item.id)}}>
+              Reject
+            </Button>
           </Col>
-          </Row>
-        </Card.Body>
-        </Card>
-      );
-    
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
 
-export default TrustRequestItem 
+export default TrustRequestItem;
