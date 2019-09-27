6cdb880f95050c857f900dc2d82cef2de0392b23
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2019-09-27 18:12:06 +0200

    Adapted for axios request

--- demo-api-server/users/controller.js
+++ demo-api-server/users/controller.js
@@ -1,47 +1,47 @@
-const usertable = require("./model");
+const usertable = require('./model')
 
-usertable.createTable();
-const controller = {};
+usertable.createTable()
+const controller = {}
 
 controller.insert = (req, res) => {
-  const data = req.body;
-  usertable.insert(data);
-  res.send(data);
-};
+  const data = req.body.data
+  usertable.insert(data)
+  res.send(data)
+}
 
 controller.getUser = (req, res) => {
-  const id = req.params.id;
-  const response = usertable.getUserById(id);
+  const id = req.params.id
+  const response = usertable.getUserById(id)
   response.then(user => {
-    const data = user.rows;
-    res.send(data);
-  });
-};
+    const data = user.rows
+    res.send(data)
+  })
+}
 
 controller.checkUser = (req, res) => {
-  const login = req.params.login;
-  const password = req.params.password;
-  const result = usertable.checkUser(login, password);
+  const login = req.params.login
+  const password = req.params.password
+  const result = usertable.checkUser(login, password)
 
   result.then(x => {
     if (x.rows.length === 0) {
-      res.status(403).send("Wrong login or password");
+      res.status(403).send('Wrong login or password')
     } else {
-      res.status(200).send(x.rows);
+      res.status(200).send(x.rows)
     }
-  });
-};
+  })
+}
 
 controller.getUserByName = (req, res) => {
-  const name = req.params.name;
-  const result = usertable.getUserByName(name);
+  const name = req.params.name
+  const result = usertable.getUserByName(name)
   result.then(x => {
     if (x.rows.length === 0) {
-      res.status(403).send("error server");
+      res.status(403).send('error server')
     } else {
-      res.send(x.rows);
+      res.send(x.rows)
     }
-  });
-};
+  })
+}
 
-module.exports = controller;
+module.exports = controller
